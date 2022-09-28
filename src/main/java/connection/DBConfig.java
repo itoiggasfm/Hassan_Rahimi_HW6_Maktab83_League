853d0af548974c6928d18612b23a15ac2308cc0d
hrahimi <hassan.rahimi@live.com>
2022-09-28 10:08:10 +0330

    Database, tables and connection package created

---
+++ src/main/java/connection/DBConfig.java
@@ -0,0 +1,9 @@
+package connection;
+
+public class DBConfig {
+
+        public static final String url = "jdbc:postgresql://localhost/league_db";
+        public static final String user = "postgres";
+        public static final String password = "99497";
+
+}
